7f15141cb18e735db33fe4fc050d88ee4bc9c6b0
Sprenger___ <gcddllcm@gmail.com>
2021-09-19 18:04:37 +0700

    add slope calculator
    add slope calculator, and also with the numerator and denominator to be construct the linear eq

--- main.py
+++ main.py
@@ -14,3 +14,35 @@ def addPoints():
     z9 = (2.39, 6.39);
     listOfPoints = [z1, z2, z3, z4, z5, z6, z7, z8, z9];
     return listOfPoints;
+
+
+
+def calculateSlope(point1, point2):
+    slope = fr(
+        fr(point2[1] - point1[1]).limit_denominator(), 
+        fr(point2[0] - point1[0]).limit_denominator());
+    
+    numerator = slope.numerator;
+    denominator = slope.denominator;
+    
+    return [slope, numerator, denominator];
+
+
+
+# def constructLinearEq(point1, point2):
+#     # y-y1 = m (x-x1)
+#     # y = mx - mx1 + y1
+#     # -mx + y = -mx1 + y1
+    
+    
+    
+    
+
+
+
+if __name__ == '__main__':
+    a = fr(5, 9);
+    b = fr(1);
+    print(a.denominator);
+    print(a.numerator);
+    print(b.denominator);
