4dab1cb82106fd379bf6cc90f7657566d22d5011
Sprenger___ <gcddllcm@gmail.com>
2021-09-19 17:49:07 +0700

    addPoints
    function to input knot-points

---
+++ main.py
@@ -0,0 +1,16 @@
+from fractions import Fraction as fr;
+
+
+
+def addPoints(): 
+    z1 = (0, 0);
+    z2 = (9, 4);
+    z3 = (5, 8);
+    z4 = (2, 5);
+    z5 = (1, 7);
+    z6 = (9, 7);
+    z7 = (5, 1);
+    z8 = (0, 6);
+    z9 = (2.39, 6.39);
+    listOfPoints = [z1, z2, z3, z4, z5, z6, z7, z8, z9];
+    return listOfPoints;
