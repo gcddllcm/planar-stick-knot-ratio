b063abdbe036784a5a47750a26806d23c79c2062
Sprenger___ <gcddllcm@gmail.com>
2021-09-19 21:52:43 +0700

    All EQ
    add a function that generate all linear eqs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fractions import Fraction as fr;
+from fractions import Fraction as fr
 
 
-
+# add knot vertices along with the direction
 def addPoints(): 
     z1 = (0, 0);
     z2 = (9, 4);
@@ -13,6 +13,7 @@ def addPoints():
     z8 = (0, 6);
     z9 = (2.39, 6.39);
     listOfPoints = [z1, z2, z3, z4, z5, z6, z7, z8, z9];
+    
     return listOfPoints;
 
 
@@ -20,7 +21,8 @@ def addPoints():
 def calculateSlope(point1, point2):
     slope = fr(
         fr(point2[1] - point1[1]).limit_denominator(), 
-        fr(point2[0] - point1[0]).limit_denominator());
+        fr(point2[0] - point1[0]).limit_denominator()
+        );
     
     return slope;
 
@@ -38,11 +40,26 @@ def constructLinearEq(point1, point2):
     for i in range(0, 3, 1):
         if linearCoeff[i].denominator == 1:
             linearCoeff[i] = linearCoeff[i].numerator;
+            
     return linearCoeff;
+
+
+
+def generateAllLinearEq(listOfAllPoints):
+    listOfAllLinearEq = [];
+    for i in range(0, len(listOfAllPoints), 1):
+        for j in range(0, len(listOfAllPoints), 1):
+            if (j - i == 1) or (i == 0 and j == len(listOfAllPoints)-1):
+                listOfAllLinearEq.append(
+                    constructLinearEq(listOfAllPoints[i], listOfAllPoints[j])
+                    );
     
+    return listOfAllLinearEq;
+                
+                
     
     
 
 if __name__ == '__main__':
     listOfPoints = addPoints();
-    print(constructLinearEq(listOfPoints[0], listOfPoints[1]));
+    print(generateAllLinearEq(listOfPoints));
